844c47026a6517e775e6fe2f7b5866989f6e15e0
unknown <wangli@wangli-PC1.tencent.com>
2014-06-16 20:33:24 +0800

    opt

--- tengmap/tengmap/tengcamera.py
+++ tengmap/tengmap/tengcamera.py
@@ -5,13 +5,17 @@ import pygame
 from pygame.locals import *
 pygame.init()
 
+import tengmap
 class tengCamera:
     def __init__(self, x, y, width=640, height=480):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
-        self.screen = pygame.display.set_mode((self.width, self.height), 0, 32)
-    def display(self):
+
+    def setmap(self, tmap):
+        self.tmap = tmap
+
+    def display(self, screen):
         
         
--- tengmap/tengmap/tengmap.py
+++ tengmap/tengmap/tengmap.py
@@ -9,15 +9,37 @@ import json
 import tenghero
 
 class mapunit:
-    def __init__(self, img_dir, indx, indy, width=32, height=32):
+    def __init__(self, img_dir, indx, indy, mapx, mapy, width=32, height=32):
         self.img_dir = img_dir
         self.indx = indx
         self.indy = indy
+        self.mapx = mapx
+        self.mapy = mapy
         self.width = width
         self.height = height
 
+    def setimg(self, img):
+        self.img = img
+
+    def display(self, delta_x, delta_y, screen):
+        tx = self.mapx - delta_x
+        ty = self.mapy - delta_y
+        swidth = screen.get_width()
+        sheight = screen.get_height()
+        points = [{'x':tx,'y':ty},
+                  {'x':tx+self.width,'y':ty},
+                  {'x':tx,'y':ty+self.height},
+                  {'x':tx+self.width,'y':ty+self.height}
+                 ]
+        for point in points:
+            if delta_x <= point['x'] <= delta_x+swidth and delta_y <= point['y'] <= delta_y+sheight:
+                break
+        else:
+            return
+        screen.blit(self.img, (tx, ty), (self.indx*self.width, self.indy*self.height, self.width, self.height))
+
 class tengmap:
-    def __init__(self, info_dir, r_dir_list):
+    def __init__(self, info_dir='mapinfo', r_dir_list = ['./pic/map.png']):
         self.info_dir = info_dir
         self.r_dir_list = r_dir_list
         self.imgs = self.loadImgs(self.r_dir_list)
@@ -25,13 +47,16 @@ class tengmap:
 
         self.default_unit = mapunit(**self.info['default_unit'])
         self.unit_list = self.loadUnits(self.info['unit_list'])
-        self.width = self.info['width']
-        self.height = self.info['height']
-        #self.camera_x = self.info['camera_x']
-        #self.camera_y = self.info['camera_y']
-        #self.camera_width = self.info['camera_width']
-        #self.camera_height = self.info['camera_height']
-        #self.hero = tenghero.tengHero(self.info['hero'])
+        self.default_unit.setimg(self.imgs[self.default_unit.img_dir])
+        for unit in self.unit_list:
+            unit.setimg(self.imgs[unit.img_dir])
+
+        self.width_cnt = self.info['width_cnt']
+        self.height_cnt = self.info['height_cnt']
+
+        self._map =[[self.default_unit for i in xrange(self.height_cnt)] for j in xrange(self.width_cnt)]
+        for unit in self.unit_list:
+            self._map[unit.mapx][unit.mapy] = unit
 
     def loadImgs(self, r_dir_list):
         imgs = {}
@@ -53,7 +78,23 @@ class tengmap:
 
     def loadUnits(self, ulist):
         unit_list = []
-        for item in ulist
+        for item in ulist:
             unit = mapunit(**item)
             unit_list.append(unit)
         return unit_list
+
+    def display(self, delta_x, delta_y, screen):
+        for arr in self._map:
+            for unit in arr:
+                unit.display(delta_x, delta_y, screen)
+
+if '__main__' == __name__:
+    screen = pygame.display.set_mode((640, 480), 0, 32)
+    clock = pygame.time.Clock()
+    tmap = tengmap()
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                exit()
+        pygame.display.update()
+        clock.tick(50)#fps 120
